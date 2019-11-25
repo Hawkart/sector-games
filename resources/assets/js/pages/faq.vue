17f68eff9de7451ed57cf206a7c0271ecf6fc784
unknown <hawkart@ramler.ru>
2019-11-25 11:29:34 +0300

    Lang en

--- resources/assets/js/pages/faq.vue
+++ resources/assets/js/pages/faq.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h3 class="nk-decorated-h-2">Часто задаваемые вопросы</h3>
+
+        <!--<h3 class="nk-decorated-h-2">Часто задаваемые вопросы</h3>
         <ol>
             <li><span class="text-white">Как создать свою команду?</span><br />
             В “Личном кабинете” слева нажмите на кнопку “Собери Команду”, далее в меню
@@ -33,6 +34,52 @@
             <li><span class="text-white">Как будут назначаться матчи? Как мы узнаем, когда мы играем, будет ли какое нибудь оповещение?</span><br />
                 Назначение матчей определяется турнирной платформой в соответствии с расписанием Турнира. Оповещение о назначении матчей будет приходить на электронную почту, указанную при регистрации.
             </li>
+        </ol>-->
+
+
+        <h3 class="nk-decorated-h-2">FAQ</h3>
+        <ol>
+            <li>
+                <span class="text-white">How to create your own team?</span><br />
+                In the “My Account” on the left, click on the “Build Team” button, then fill in the information about the team in the menu.
+            </li>
+            <li>
+                <span class="text-white">How to join an existing team?</span><br />
+                Go to the list of commands <a href="https://sector.games/teams">https://sector.games/teams</a>a>, find the team, click the "Join the team" button.
+            </li>
+            <li>
+                <span class="text-white">How to invite players to the team?</span><br />
+                From your Personal Account, send an invitation to the team by e-mail, publish the invitations on your VKontakte wall or in SteamCommunity. After players register on the site <a href="https://sector.games">https://sector.games/</a>,
+                select players in the <a href="https://sector.games/players">https://sector.games/players</a> list and click the "Invite to Team" button.
+            </li>
+            <li>
+                <span class="text-white">How do I know that I was invited to the team?</span><br />
+                The team captain invites players to the team. You will receive an email notification of the invitation you provided during registration. If you agree to join the team - confirm in the menu in your personal account.
+            </li>
+            <li>
+                <span class="text-white">How many players should be in a team?</span><br />
+                A DOTA2 team can have only 5 players. Spare players and replacements are not provided.
+            </li>
+            <li>
+                <span class="text-white">How to register with a “ready-made team”?</span><br />
+                Each of the players of the "finished team" must register on the site <a href="https://sector.games">https://sector.games/</a>. Then the team captain creates a team with its name on the site and sends invitations to all players. As soon as all players accept them, the team is considered formed.
+            </li>
+            <li>
+                <span class="text-white">How is the registration for the tournament?</span><br />
+                Registration for the tournament is carried out by the team captain. When registering for the tournament, it is checked that all the team players indicated their real names and surnames, entered information about their SteamID and VKontakte accounts, only in this case the team will be registered for the Tournament.
+            </li>
+            <li>
+                <span class="text-white">How to check if my team is registered for the Tournament?</span><br />
+                When a team is registered for the Tournament, all team players will receive notification of registration by email. To make sure that the team is registered to the Tournament - go to the list of Tournaments https://sector.games/tournaments, select the Tournament you are interested in, go to the "Teams" section, find your team in it.
+            </li>
+            <li>
+                <span class="text-white">How to see what other teams are participating in the Tournament?</span><br />
+                All teams registered for the Tournament are displayed in the “Teams” section on the page of this Tournament.
+            </li>
+            <li>
+                <span class="text-white">How will matches be scheduled? How do we know when we play, will there be any kind of alert?</span><br />
+                The purpose of the matches is determined by the tournament platform in accordance with the schedule of the Tournament. A notification of the appointment of matches will be received by e-mail specified at registration.
+            </li>
         </ol>
     </div>
 </template>
@@ -45,7 +92,7 @@
         layout: 'default',
 
         metaInfo () {
-            return { title: 'Часто задаваемые вопросы' }
+            return { title: 'FAQ' }
         },
 
         computed:{
